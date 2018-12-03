c9c73ea6513061f7241db140dddb029e067552ad
pro1 <pro1@MBYdomain.local>
2018-12-03 14:39:52 +0200

    finished the angular cli lesson

--- workspace/projects/hello-world/src/app/app.component.spec.ts
+++ workspace/projects/hello-world/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'workspace'`, () => {
+  it(`should have as title 'hello-world'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('workspace');
+    expect(app.title).toEqual('hello-world');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to workspace!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to hello-world!');
   });
 });
