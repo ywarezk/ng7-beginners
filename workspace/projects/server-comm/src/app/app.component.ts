d0e5ae673f9bfde4b77e4ec51998d4a19c7a6216
pro1 <pro1@MBYdomain.local>
2018-12-09 12:23:42 +0200

    added the async lesson and the server comm

---
+++ workspace/projects/server-comm/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'server-comm';
+  tasks : any[];
+
+  constructor(private _http: HttpClient) {}
+
+  ngOnInit() {
+    this._http.get('https://nztodo.herokuapp.com/api/task/?format=json').subscribe((json: any) => {
+      this.tasks = json;
+    }, (err: Error) => {});
+  }
+}
